4a80a0e8b66d0636466e7022174669a3feac5732
isabelabarros-o <164945571+isabelabarros-o@users.noreply.github.com>
2024-04-02 22:55:20 -0300

    Add files via upload

---
+++ ContaBanco.java
@@ -0,0 +1,24 @@
+package isabela
+
+import java.util.Scanner;
+
+public class ContaBanco {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o nome do cliente: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque.");
+    }
+}
